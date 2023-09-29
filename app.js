c2f1bec78a882927d14dace9b60023b5b2064e3d
Muhammad Saqib Ilyas <msaqib@gmail.com>
2023-09-29 17:21:46 +0500

    Implementation complete.

--- app.js
+++ app.js
@@ -14,18 +14,27 @@ btnStart.addEventListener('click', startStop)
 btnReset.addEventListener('click', reset)
 
 function startStop() {
-    console.log('Start')
-    timerInterval = setInterval(() => {
+    if (state === 0) {
+        timerInterval = setInterval(() => {
         time = time + 1
         tim.textContent = time
-    }, 1000)    
-    btnStart.textContent = 'Pause'
-    btnReset.disabled = false
+        }, 1000)    
+        btnStart.textContent = 'Pause'
+        btnReset.disabled = false    
+        state = 1
+    }
+    else {
+        clearInterval(timerInterval)
+        btnStart.textContent = 'Resume'
+        state = 0
+    }
+    
 }
 
 function reset() {
-    console.log('Stop')
     clearInterval(timerInterval)
     time = 0
     tim.textContent = time
+    btnStart.textContent = 'Start'
+    state = 0
 }
